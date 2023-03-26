8d5e6c6ea1b0c4010d2362a7efbb53e2bbed8a39
youngcheon <kimyoungcheon97@gmail.com>
2023-03-26 12:37:08 +0900

    test

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  console.log(`listening on port ${port}`);
+    const app = await NestFactory.create(AppModule);
+    const port = process.env.PORT || 3000;
+    const config = new DocumentBuilder()
+        .setTitle('select api')
+        .setDescription('Sleact 개발을 위한 API 문서')
+        .setVersion('1.0')
+        .addCookieAuth('connect.sid')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
 
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
+    await app.listen(port);
+    console.log(`listening on port ${port}`);
+
+    if (module.hot) {
+        module.hot.accept();
+        module.hot.dispose(() => app.close());
+    }
 }
 bootstrap();
